2171d27be920cb59138515cae1ee06c6acb244b6
Daniel Rond de Jesus dos Santos <166262041+DanielRond@users.noreply.github.com>
2026-06-28 18:34:58 -0300

    Time: 20 ms (64.26%), Space: 25.4 MB (70.48%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,14 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = [1] * len(nums)
+        prefix,sufix = 1,1
+        for i in range(len(nums)):
+            answer[i] = prefix
+            prefix *= nums[i]   
+        for i in range(len(nums)-1, -1, -1):
+            answer[i] *= sufix
+            sufix *= nums[i]
+        return answer
+
+
+           
